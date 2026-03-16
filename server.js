5154c02810b3372572483815178becf09c748222
bonnie <bonnielin@ryzo.io>
2026-03-16 15:45:55 +0800

    Prepare docs-based GitHub Pages deployment

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const staticDir = path.join(__dirname, "docs");
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(staticDir));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(staticDir, "index.html"));
 });
 
 app.listen(PORT, () => {
